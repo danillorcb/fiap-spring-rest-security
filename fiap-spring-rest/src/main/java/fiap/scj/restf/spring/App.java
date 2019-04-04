2fe6ffd6a627f7fe194b011f981cb216f09062d5
Danillo Ramos <nillorcb21@gmail.com>
2019-04-03 22:06:24 -0300

    Initial commit

---
+++ fiap-spring-rest/src/main/java/fiap/scj/restf/spring/App.java
@@ -0,0 +1,15 @@
+package fiap.scj.restf.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.plugin.core.config.EnablePluginRegistries;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
